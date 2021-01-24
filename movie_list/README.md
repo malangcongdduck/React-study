1858f4ef8ba919643194bb96c1223218b2d66474
malangcongdduck <cielciel107@gmail.com>
2021-01-24 22:47:53 +0900

    Using React JSX

---
+++ movie_list/README.md
@@ -0,0 +1,4 @@
+#App Study
+##2021.01.23~
+
+Study app development using React Js
---
+++ movie_list/src/Wolf.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Wolf(){
+    return <h3>I love wolf</h3>;
+}
+
+export default Wolf;
